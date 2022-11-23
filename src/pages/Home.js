3f3fe1b217ad9da115a2db7dc37e4223eeac72a6
Ilie1985 <ilie.marian32@yahoo.com>
2022-11-23 15:18:43 +0000

    Home setup

---
+++ src/pages/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <h2> Home Page</h2>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus dolore vero voluptatibus incidunt, eum fuga non! Ipsa similique sint id vel? Ut laudantium aspernatur officia quos in molestiae quas dolorem?
+      Doloremque nobis quia culpa maiores aperiam veritatis placeat quos, soluta molestiae vel. Beatae blanditiis voluptas placeat repellat expedita? Amet, repellendus aspernatur nisi quis laboriosam sint maiores assumenda nesciunt dolor doloremque?</p>
+    </div>
+  );
+};
+
+export default Home;
