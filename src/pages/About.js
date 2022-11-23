5d249f566563b39be3ba6b19b9659b0d2f417545
Ilie1985 <ilie.marian32@yahoo.com>
2022-11-23 15:20:23 +0000

    about setup

--- src/pages/About.js
+++ src/pages/About.js
@@ -3,7 +3,9 @@ import React from 'react'
 const About = () => {
   return (
     <div>
-      h2
+      <h2>About Page</h2>
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odit magnam atque placeat, dolore error libero non! Voluptatibus minima tenetur placeat. Dolorem numquam iste quas veritatis illo minus deserunt eveniet qui.
+      Ipsum quis quasi minus, dicta praesentium exercitationem dignissimos delectus ducimus nemo dolorem itaque hic modi quaerat at obcaecati sint aliquam, rerum magnam voluptatem nesciunt debitis reprehenderit facere tempora libero? Saepe?</p>
     </div>
   )
 }
