4f388a306a8d769917a73f66972cd150cebd55df
Ilie1985 <ilie.marian32@yahoo.com>
2022-11-23 15:19:47 +0000

    about setup

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -2,7 +2,11 @@ import React from 'react'
 
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <div>
+      <h2>Contact Page</h2>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Error distinctio molestiae et maiores sequi dolore voluptatum a voluptatem voluptatibus, ullam labore esse cupiditate officiis itaque rerum, impedit quod amet ipsa.
+      Est quam, architecto explicabo aut cupiditate recusandae, odio eum omnis sint nesciunt quibusdam doloremque beatae velit veritatis eius perferendis. Soluta laboriosam at accusamus adipisci voluptas nihil molestiae vero, accusantium error?</p>
+    </div>
   )
 }
 
